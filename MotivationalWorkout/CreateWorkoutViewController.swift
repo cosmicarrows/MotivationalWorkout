6ed2d007e167944004a95edb70b5a395845af507
Laurence Wingo <laurence@cosmicarrows.com>
2018-05-27 22:01:03 -0400

    Implement Outlets and Actions for WorkoutViewController

---
+++ MotivationalWorkout/CreateWorkoutViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CreateWorkoutViewController.swift
+//  MotivationalWorkout
+//
+//  Created by Laurence Wingo on 5/27/18.
+//  Copyright © 2018 Cosmic Arrows, LLC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CreateWorkoutViewController: UIViewController {
+    
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var progressLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var toggleButton: UIButton!
+    
+    @IBAction func toggleWorkout(_ sender: UIButton) {
+    
+    }
+}
